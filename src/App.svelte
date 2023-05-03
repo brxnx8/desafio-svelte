8fb2f67d6dd0965c28c1ee6080f68394449598ba
Bruno oliveira <brunogabriel2608@gmail.com>
2023-05-03 14:39:32 -0300

    feito esboço do formulario

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,36 @@
-<script lang="ts">
+<script lang="ts" context="module">
+  import Form from "./lib/form.svelte";
+
+  export interface IcharactersPassword{
+    hasUpercaseLetter: boolean;
+    hasLowercaseLetter: boolean;
+    hasNumbers: boolean;
+    hasSimbols: boolean;
+  }
+
+  let password: string = ''
+
+  let charactersPassword: IcharactersPassword = {
+    hasUpercaseLetter: true,
+    hasLowercaseLetter: true,
+    hasNumbers: true,
+    hasSimbols: true,
+  }
+  
+
+
 </script>
 
 <main class="flex justify-center items-center bg-slate-900 w-full h-screen">
  
-  <main class="flex flex-col justify-center items-center text-white space-y-3 w-5/12">
+  <section class="flex flex-col justify-center items-center text-white space-y-3 w-5/12">
     <h1 class="">Password Generate</h1>
     <div class="bg-gray-800 rounded-sm space-x-2 w-full p-5 flex justify-between" >
-      <span>teste</span>
+      <span>{password}</span>
       <input type="checkbox" />
     </div>
-    <form class="bg-gray-800 rounded-sm lex flex-col space-y-3 w-full p-5" >
-      <div class="space-y-2 flex-col flex">
-        <div class="flex justify-between">
-          <span>Character length</span>
-          <span>10</span>
-        </div>
-        <input type="range" />
-      </div>
-    
-      <div class="flex flex-col items-start space-y-2">
-        <div>
-          <input type="checkbox" />
-          <label for="">wef</label>
-        </div>
-        <div>
-          <input type="checkbox" />
-          <label for="">wef</label>
-        </div>
-        <div>
-          <input type="checkbox" />
-          <label for="">wef</label>
-        </div>
-        <div>
-          <input type="checkbox" />
-          <label for="">wef</label>
-        </div>
-        <div class="flex justify-between w-full px-5 py-3 bg-gray-900">
-          <span>Character length</span>
-          <span>10</span>
-        </div>
-      </div>
-      <button class="bg-green-500 h-12 w-full">Generate</button>
-    </form>
-  </main>
+    <Form {charactersPassword}/>
+  </section>
   
 </main>
 
---
+++ src/lib/form.svelte
@@ -0,0 +1,40 @@
+<script type="module" lang="ts">
+    import type { IcharactersPassword } from "../App.svelte";
+    
+
+    export let charactersPassword: IcharactersPassword;
+</script>
+
+<form class="bg-gray-800 rounded-sm lex flex-col space-y-3 w-full p-5" >
+    <div class="space-y-2 flex-col flex">
+      <div class="flex justify-between">
+        <span>Character length</span>
+        <span>10</span>
+      </div>
+      <input type="range" />
+    </div>
+  
+    <div class="flex flex-col items-start space-y-2">
+      <div>
+        <input type="checkbox" bind:checked="{charactersPassword.hasUpercaseLetter}" />
+        <label for="">Include uppercase letters</label>
+      </div>
+      <div>
+        <input type="checkbox" bind:checked="{charactersPassword.hasLowercaseLetter}" />
+        <label for="">Include lowercase letters</label>
+      </div>
+      <div>
+        <input type="checkbox" bind:checked="{charactersPassword.hasNumbers}" />
+        <label for="">Include numbers</label>
+      </div>
+      <div>
+        <input type="checkbox" bind:checked="{charactersPassword.hasNumbers}" />
+        <label for="">Include simbols</label>
+      </div>
+      <div class="flex justify-between w-full px-5 py-3 bg-gray-900">
+        <span>strength</span>
+        <span>10</span>
+      </div>
+    </div>
+    <button class="bg-green-500 h-12 w-full">Generate</button>
+  </form>
