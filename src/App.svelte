d12998baf5b3f95939675b21159dd587add91eef
Bruno oliveira <brunogabriel2608@gmail.com>
2023-05-03 14:11:41 -0300

    feito esboço do html

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,48 @@
 <script lang="ts">
 </script>
 
-<main class="flex flex-col justify-center items-center bg-slate-900 text-white w-full h-screen space-y-2">
+<main class="flex justify-center items-center bg-slate-900 w-full h-screen">
  
-  <h1 class="">Password Generate</h1>
-  <div class="bg-gray-800 rounded-sm p-3 space-x-2" >
-    <span>teste</span>
-    <input type="checkbox" />
-  </div>
+  <main class="flex flex-col justify-center items-center text-white space-y-3 w-5/12">
+    <h1 class="">Password Generate</h1>
+    <div class="bg-gray-800 rounded-sm space-x-2 w-full p-5 flex justify-between" >
+      <span>teste</span>
+      <input type="checkbox" />
+    </div>
+    <form class="bg-gray-800 rounded-sm lex flex-col space-y-3 w-full p-5" >
+      <div class="space-y-2 flex-col flex">
+        <div class="flex justify-between">
+          <span>Character length</span>
+          <span>10</span>
+        </div>
+        <input type="range" />
+      </div>
+    
+      <div class="flex flex-col items-start space-y-2">
+        <div>
+          <input type="checkbox" />
+          <label for="">wef</label>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label for="">wef</label>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label for="">wef</label>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label for="">wef</label>
+        </div>
+        <div class="flex justify-between w-full px-5 py-3 bg-gray-900">
+          <span>Character length</span>
+          <span>10</span>
+        </div>
+      </div>
+      <button class="bg-green-500 h-12 w-full">Generate</button>
+    </form>
+  </main>
   
 </main>
 
