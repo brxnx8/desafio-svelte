ec7df0b2fee285f457981fd015358fcbc597a879
Bruno oliveira <brunogabriel2608@gmail.com>
2023-05-03 13:32:39 -0300

    inicializacao do projeto

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<main class="flex flex-col justify-center items-center bg-slate-900 text-white w-full h-screen space-y-2">
+ 
+  <h1 class="">Password Generate</h1>
+  <div class="bg-gray-800 rounded-sm p-3 space-x-2" >
+    <span>teste</span>
+    <input type="checkbox" />
+  </div>
+  
+</main>
+
+<style>
+  
+</style>
